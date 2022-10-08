2d0a7a8bc20c61f3c937cac7e6c94c4fae1e15ac
Yazhmog <88186277+YazZHh@users.noreply.github.com>
2022-10-08 21:50:40 +0400

    Update CanalBot-Tsundere-Raws.py
    - last_torrent_proceed isn't changed anymore when no action is made to the raw file (when the script skip encoding or copying it because there's already an output file existing)
    - Fix a bug that made the latest torrent added to the rss page "already added"
    - Erai raws version is comming later

--- CanalBot-Tsundere-Raws.py
+++ CanalBot-Tsundere-Raws.py
@@ -20,7 +20,7 @@ class settings:
     user = "admin"                                          # Username of your qBittorrent Web UI
     password = "adminadmin"                                 # Password for the Web ui
     webui_link = "http://localhost:8080"                    # Change "https://link-to-my-web.ui:PORT" to your actual web domain, please note that you can also use "http://localhost:8080" if you don't have a domain name
-    rss_link = "https://nyaa(.)si/?page=rss&u=Tsundere-Raws"    # Just remove the "()" and run the script
+    rss_link = 'https://nyaa(.)si/?page=rss&u=Tsundere-Raws'# Just remove the "()" and run the script
 
 request_count = 0                           # Do not modify the following lines
 rss_search_results = []
@@ -50,14 +50,14 @@ class txt:
             temp = open(self.file_name, "r")
         except FileNotFoundError:
             temp = open(self.file_name, "x")
-            temp = open(self.file_name, "r")  
+            temp = open(self.file_name, "r")
         return list(temp.read().split("\n"))
 
     def load_data(self):
-        if self.type == 1:      # Type = 1 -> anime_list.txt
+        if self.type == 1:                                              # Type = 1 -> anime_list.txt
             self.data = self.read_txt_to_list_of_lists()
             self.keywords = list(anime[0] for anime in self.data)
-        elif self.type == 2:    # Type = 2 -> processed_list.txt
+        elif self.type == 2:                                            # Type = 2 -> processed_list.txt
             self.data = self.read_txt_to_list()
 
     def get_info(self, file_name):
@@ -94,7 +94,7 @@ class txt:
         return False
 
 
-def index_verify(file_name, index):                                     # Theses three index_things functions return the position of the index of the file (Here used to search " - ")
+def index_verify(file_name, index):                                     # Theses three following index_things functions work together and return the position of the index of the file (Here used to search " - ")
     try:                                                                # It finds the first occurence of the index, then verify it by trying to int() the two next characters (the two next characters being the episode number)
         int(file_name[index + 3:index + 5])
         return True
@@ -131,7 +131,7 @@ def rss_search(keyword, quality):
             torrent_name = entries[entry_number].title
             if entries[entry_number].title.find(quality) != -1:                                                 # Then search for the right quality
                 rss_torrent_title = entries[entry_number].title
-                if rss_torrent_title.find('.mkv') != -1 and rss_torrent_title.find('VOSTFR') != -1:  # "if rss_torrent_title.find("HEVC") == -1 and rss_torrent_title.find("Multiple Subtitle") != -1:" : Get rid of the shitty Erai-Raws's HEVC and ENG subs only torrents.
+                if rss_torrent_title.find('.mkv') != -1 and rss_torrent_title.find('VOSTFR') != -1:             # "if rss_torrent_title.find("HEVC") == -1 and rss_torrent_title.find("Multiple Subtitle") != -1:" : Get rid of the shitty Erai-Raws's HEVC and ENG subs only torrents.
                     torrent_index = search_index(rss_torrent_title)
                     if check_if_added(torrent_index, keyword, rss_torrent_title[torrent_index + 3:torrent_index + 5]) == False:
                         qb.download_from_link(entries[entry_number].link)
@@ -204,7 +204,8 @@ def search_for_new_torrents():
     crash = False
     try:
         for keyword in anime_list.keywords:
-            rss_search(keyword, settings.quality)
+            if keyword != "":
+                rss_search(keyword, settings.quality)
     except:
         # print("\033[1;91mError while searching for new torrents / Error while retrieving Torrents info from the qBittorrent Web UI\033[0m")
         # Line above commented because the error can appears when no crash has happen (likely because the request() function takes a bit of time to execute sometimes)
@@ -217,111 +218,110 @@ def timeout_search():   # I need to get rid of this function
     action_thread.join(timeout=10)
     stop_event.set()
 
-qb_request()
-anime_list = txt("anime_list.txt", 1)
-processed_list = txt("processed_list.txt", 2)
+if __name__ == "__main__":
+    qb_request()
+    anime_list = txt("anime_list.txt", 1)
+    processed_list = txt("processed_list.txt", 2)
 
-while True:     # Main infinite loop start
-    anime_list.load_data()
-    processed_list.load_data()
+    while True:     # Main infinite loop start
+        anime_list.load_data()
+        processed_list.load_data()
 
-    if crash == True:
-        qb_request()
+        if crash == True:
+            qb_request()
 
-    source_rss = rss_request()
-        
-    if len(source_rss) == 7:          # did some tests, and lenght = 7 if RSS request success
-        fail_count = 0
-        request_count += 1
-        not_found = []
-        encode = False
+        source_rss = rss_request()
+            
+        if len(source_rss) == 7:          # did some tests, and lenght = 7 if RSS request success
+            request_count += 1
+            not_found = []
+            encode = False
 
-        if crash == False:
-            previous_rss_search_results = rss_search_results
-            rss_search_results = []
+            if crash == False:
+                previous_rss_search_results = rss_search_results
+                rss_search_results = []
 
-        print("\033[1mLast torrent added in the RSS feed\033[0m :", source_rss.entries[0].title)
-        timeout_search()
+            print("\033[1mLast torrent added in the RSS feed\033[0m :", source_rss.entries[0].title)
+            timeout_search()
 
-        if crash == False:
-            not_found_list = ', '.join(not_found)
-            if len(not_found_list) != 0:
-                print("No results for :", not_found_list)
-            try:
-                torrents_info = qb.torrents()
-            except:
-                print("\033[1;91mError while retrieving Torrents info from the qBittorrent Web UI\033[0m")
-                crash = True
+            if crash == False:
+                not_found_list = ', '.join(not_found)
+                if len(not_found_list) != 0:
+                    print("No results for :", not_found_list)
+                try:
+                    torrents_info = qb.torrents()
+                except:
+                    print("\033[1;91mError while retrieving Torrents info from the qBittorrent Web UI\033[0m")
+                    crash = True
 
-        if last_torrent_proceed != None:
-            print("\033[92mLast torrent proceed\033[0m :", last_torrent_proceed)
+            if last_torrent_proceed != None:
+                print("\033[92mLast torrent proceed\033[0m :", last_torrent_proceed)
 
-        if request_count != 1:
-            print("Total run time :", time_calculation(round((time.time() - start_time))))
+            if request_count != 1:
+                print("Total run time :", time_calculation(round((time.time() - start_time))))
 
-        if crash == False:
-                for torrent in torrents_info:
-                    file_name = torrent['name']
-                    if file_name.find(settings.rss_link[28:-1]) != -1 and not processed_list.check_if_processed(file_name) and anime_list.check_if_on_the_list(file_name):
-                        if torrent['state'] != 'downloading' and torrent['state'] != 'stalledDL':
+            if crash == False:
+                    for torrent in torrents_info:
+                        file_name = torrent['name']
+                        if file_name.find(settings.rss_link[28:-1]) != -1 and not processed_list.check_if_processed(file_name) and anime_list.check_if_on_the_list(file_name):
+                            if torrent['state'] != 'downloading' and torrent['state'] != 'stalledDL':
 
-                            file_info = anime_list.get_info(file_name)              # Get infos from anime_list.txt
-                            anime_name = file_info[3]
-                            index_rank = search_index(file_name)
-                            episode_number = file_name[index_rank + 3:index_rank + 5]
-                            input_file_name = file_name.replace(" ", "\ ").replace("(", "\(").replace(")", "\)")
-                            point_name = anime_name.replace(" ", ".")
-                            output_file_name = f'{point_name}.s{file_info[2]}e{episode_number}.{settings.suffix}'
+                                file_info = anime_list.get_info(file_name)                                              # Get infos from anime_list.txt
+                                anime_name = file_info[3]
+                                index_rank = search_index(file_name)
+                                episode_number = file_name[index_rank + 3:index_rank + 5]
+                                input_file_name = file_name.replace(" ", "\ ").replace("(", "\(").replace(")", "\)")    # Small changes needed in order to match the syntax of a file name in a linux command 
+                                point_name = anime_name.replace(" ", ".")
+                                output_file_name = f"{point_name}.s{file_info[2]}e{episode_number}.{settings.suffix}"
 
-                            if settings.auto_encode == True:    # Encoding the file
-                                os.system(f'mkdir -p {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}')    # Create a folder for the output file, if wasn't already
-                                if os.path.exists(f"{settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'}"):
-                                    print(f"Skipping encoding for {file_name} : output file already exists")
+                                if settings.auto_encode == True:    # Encoding the file
+                                    os.system(f"mkdir -p {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}")    # Create a folder for the output file, if wasn't already
+                                    if os.path.exists(f"{settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'}"):
+                                        print(f"\033[35mSkipping encoding for {file_name} : output file already exists\033[0m")
+                                    else:
+                                        print(f"\033[96mEncoding {file_name} to {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'}\033[0m..")
+                                        os.system(f"HandBrakeCLI -i {settings.torrents_location}/{input_file_name} -o {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'} {settings.handbrake_settings}")
+                                        print("\033[92mDone !\033[0m")
+                                        last_torrent_proceed = file_name
+                                        encode = True
+
+                                else:                               # Copying the file to the destination folder
+                                    if os.path.exists(f"{settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}"):
+                                        print(f"\033[35mSkipping copying {file_name} : output file already exists\033[0m")
+                                    else:
+                                        print(f"\033[96mCopying {file_name} to {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}..\033[0m")
+                                        os.system(f"cp {settings.torrents_location}/{input_file_name} {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}")
+                                        print("\033[92mDone !\033[0m")
+                                        last_torrent_proceed = file_name
+
+                                processed_list.data.append(file_name)
+                                processed_list.write_to_txt()
+
+                                if settings.auto_encode == True:    # Giving the file the right permissions
+                                    os.system(f"sudo chown {settings.linuxuser} {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'}")
+                                    os.system(f"sudo chmod 775 {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'}")
                                 else:
-                                    print(f"\033[96mEncoding {file_name} to {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'}\033[0m..")
-                                    os.system(f"HandBrakeCLI -i {settings.torrents_location}/{input_file_name} -o {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'} {settings.handbrake_settings}")
-                                    print("\033[92mDone !\033[0m")
-                                    encode = True
+                                    os.system(f"sudo chown {settings.linuxuser} {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}")
+                                    os.system(f"sudo chmod 775 {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}")
 
-                            else:   # Copying the file to the destination folder
-                                if os.path.exists(f"{settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}"):
-                                    print(f"Skipping copying {file_name} : output file already exists")
-                                else:
-                                    print(f"\033[96mCopying {file_name} to {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}..\033[0m")
-                                    os.system(f"cp {settings.torrents_location}/{input_file_name} {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}")
-                                    print("\033[92mDone !\033[0m")
-
-                            processed_list.data.append(file_name)
-                            processed_list.write_to_txt()
-
-                            last_torrent_proceed = file_name
-
-                            # Giving the file the right permissions
-                            if settings.auto_encode == True:
-                                os.system(f"sudo chown {settings.linuxuser} {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'}")
-                                os.system(f"sudo chmod 775 {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mp4'}")
                             else:
-                                os.system(f"sudo chown {settings.linuxuser} {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}")
-                                os.system(f"sudo chmod 775 {settings.target_directory}/animes/{file_info[1]}/s{file_info[2]}/{output_file_name + '.mkv'}")
+                                print(f"\033[31mFile {file_name} is still downloading !\033[0m")
 
-                        else:
-                            print(f'\033[31mFile {file_name} is still downloading\033[0m')
+            if request_count != 1 and settings.delete_torrents_afterwards == True:
+                clean_torrents()
 
-        if request_count != 1 and settings.delete_torrents_afterwards == True:
-            clean_torrents()
+            print(f"\n\033[4m{request_count} requests made\033[0m")
 
-        print(f'\n\033[4m{request_count} requests made\033[0m')
+            if encode == False:
+                print("Wating 7 minutes before the next request..")
+                time.sleep(420)     # Change this value (in seconds) to wait more or less before the next request
+            elif crash == True:
+                print("Wating 30 seconds before the next request..")
+                time.sleep(30)
+                qb_request()
+            else:
+                qb_request()
 
-        if encode == False:
-            print("Wating 7 minutes before the next request..")
-            time.sleep(420)     # Change this value (in seconds) to wait more or less before the next request
-        elif crash == True:
-            print("Wating 30 seconds before the next request..")
-            time.sleep(30)
-            qb_request()
-        else:
-            qb_request()
-
-    elif len(source_rss) == 5:
-        print("RSS request failed, next try in 5 seconds..")
-        time.sleep(5)
+        elif len(source_rss) == 5:
+            print("RSS request failed, next try in 5 seconds..")
+            time.sleep(5)
